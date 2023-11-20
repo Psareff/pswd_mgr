6e4f951d17b3f32dbf13d79a0a8c675d5e95b354
Psareff <104565733+Psareff@users.noreply.github.com>
2023-11-21 00:21:42 +0700

    Add files via upload

---
+++ password_server.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("File successfully opened, awaiting incoming requests\n");
+    printf("[Connection request from IP {192.168.1.131}]\n");
+    printf("[Connection established, waiting for database request]\n");
+    printf("Password requested for <google.com>, username <psareff>\n");
+    printf("Master-password check failure\n");
+    printf("Master-password check failure\n");
+    printf("Master-password check successful\n");
+    printf("[Password for <google.com>, <psareff> sent]\n")
+
+}
