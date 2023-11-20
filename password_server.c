092838fa558568fbacc23b9cc3c3ce1f807c0320
Psareff <104565733+Psareff@users.noreply.github.com>
2023-11-21 00:27:00 +0700

    Add files via upload

--- password_server.c
+++ password_server.c
@@ -1,14 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     printf("File successfully opened, awaiting incoming requests\n");
     printf("[Connection request from IP {192.168.1.131}]\n");
-    printf("[Connection established, waiting for database request]\n");
-    printf("Password requested for <google.com>, username <psareff>\n");
-    printf("Master-password check failure\n");
-    printf("Master-password check failure\n");
-    printf("Master-password check successful\n");
-    printf("[Password for <google.com>, <psareff> sent]\n")
+    printf("[Connection established, waiting for database request]\n\n");
 
+    printf("[Password get-request for <google.com>, username <psareff>]\n");
+    printf("Master-password check failure\n");
+    printf("Master-password check failure\n");
+    printf("Master-password check success\n");
+    printf("[Password for <google.com>, <psareff> sent]\n\n");
+
+    printf("[Password post-request for <yandex.ru>, username <mstr_i>\n");
+    printf("Master-password check success\n");
+    printf("[Password for <yandex.ru>, <mstr_i> written]\n\n");
+
+    printf("[Password delete-request for <google.com>, username <psareff>\n");
+    printf("Master-password check success\n");
+    printf("[Password for <google.com>, <psareff> deleted]\n\n");
+
+    printf("[Connection stopped with IP {192.168.1.131}]\n");
+    sleep(10);
 }
